7a55934bbb6422010c84d0babb10b23cb2c3b815
sma9000 <93053479+sma9000@users.noreply.github.com>
2023-10-28 19:17:46 -0400

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,5 @@
 # Author: <Salma Awan>
-# Assignment #5 - Tic-tac-toe
-# Date due: 2021-11-29
-# I pledge that I have completed this assignment without
-# collaborating with anyone else, in conformance with the
-# NYU School of Engineering Policies and Procedures on
-# Academic Misconduct.
+# Implementation of Tic-tac-toe Game
 
 ####### DO NOT EDIT CODE BELOW ########
 
